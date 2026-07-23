c07329ae520e46eafc7348f551a70792acf38583
dax <dax26d@gmail.com>
2026-07-23 15:46:04 +0800

    feat: 添加 PWA 支持 (manifest + service worker + icon)

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'sudoku-v1'
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.svg',
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('/'))
+    )
+    return
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
